8f65658af59b68ddc4b44d0c3a48d729329974cd
Enrico <nanobarbalunga@gmail.com>
2024-04-03 12:47:15 +0200

    upd index.js

--- src/index.js
+++ src/index.js
@@ -8,17 +8,29 @@ const routes = require('./routes');
 console.log(`* Creazione app express`, '...')
 const app = express();
 
+console.log(`* Creazione app express`,1, ' ...')
+
 app.use(cors());
 app.options('*', cors());
 
+console.log(`* Creazione app express`,2, ' ...')
+
 app.use(morgan(':method :url :status :user-agent - :response-time ms'));
+
+console.log(`* Creazione app express`,3, ' ...')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+console.log(`* Creazione app express`,4, ' ...')
+
 app.use('/', routes);
 
+console.log(`* Creazione app express`,5, ' ...')
+
 app.listen(process.env.PORT || 3000, function() {
+	console.log(`* Creazione app express`,6, ' ...')
 	console.log('Express app running on port ' + (process.env.PORT || 3000))
 	console.log('site => http://localhost:' + (process.env.PORT || 3000))
 });
+console.log(`* Creazione app express`,7, ' ...')
